33a0a37f8020bc806c7dfeec2cb43b9019045392
Misha Starodubets <starodubetsm@gmail.com>
2019-10-08 10:50:37 +0300

    Node.js add basic structure Router for notes/ Models for notes

---
+++ models/notes.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create a schema
+const notesSchema = new Schema({
+    title: {type: String, required: true},
+    description: String,
+    basket: Boolean,
+    created_at: Date,
+    updated_at: Date
+});
+
+const Notes = mongoose.model('Notes', notesSchema);
+
+module.exports = Notes;
