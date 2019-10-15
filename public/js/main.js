8101f6f7c464b40b61eaa54a5060ded2517a3486
Roman Zakrevskyi <romul3003@gmail.com>
2019-10-15 22:10:40 +0300

    fixed syntax errors

--- public/js/main.js
+++ public/js/main.js
@@ -23,103 +23,106 @@ if (form) {
                         data['imageId'] = file.value || '';
                         data['imageName'] = file.name || '';
 
-        console.log(data);
+                        console.log(data);
 
-        if (data.id) {
-                fetch('/notes', {
-                    method: 'PUT',
+                        if (data.id) {
+                            fetch('/notes', {
+                                method: 'PUT',
 
-                    body: formData
-                }).finally(() => {
+                                body: formData
+                            })
+                              .finally(() => {
+
+                              })
+                        }
                     }
+                    data[name] = value;
+                }
+                formData.append('data', JSON.stringify(data));
+
+                if (data.id) {
+                    fetch('/notes', {
+                        method: 'PUT',
+                        body: formData
+                    }).then(res => {
+                        window.location.href = '/';
+                    }).finally(() => {
+
+                        console.log("Go to space and Update!!!!!!");
+                    });
+
+
+                } else {
+                    fetch('/notes', {
+                        method: 'POST',
+                        body: formData,
+                    }).then(r => {
+                        console.log(r);
+                        window.location.href = '/';
+                    }).finally(() => {
+                        console.log("Go to space!!!!!!");
+                    });
+
                 }
             }
-            data[name] = value;
         }
-        formData.append('data', JSON.stringify(data));
-
-        if (data.id) {
-            fetch('/notes', {
-                method: 'PUT',
-                body: formData
-            }).then(res => {
-               window.location.href = '/';
-            }).finally(() => {
-
-                console.log("Go to space and Update!!!!!!");
-            });
 
 
-        } else {
-            fetch('/notes', {
-                method: 'POST',
-                body: formData,
-            }).then(r => {
-                console.log(r);
-               window.location.href = '/';
-            }).finally(() => {
-                console.log("Go to space!!!!!!");
-            });
+        function onBtnDeleteClick(event, id, imageId) {
+            event.preventDefault();
 
+            if (document.getElementById(id)) {
+                document.getElementById(id).remove();
+            } else {
+                // window.location.href = '/';
+            }
+
+            if (id !== null && id !== undefined && id.length > 0) {
+                fetch('/notes', {
+                    method: 'DELETE',
+                    body: JSON.stringify({id: id}),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                }).then(res => {
+                    // window.location.href = '/';
+                }).finally(() => {
+                    console.log("Go to space and delete!!!!!!");
+                });
+            }
+
+            if (imageId !== null && imageId !== undefined && imageId.length > 0) {
+
+            }
+            // }).finally(() => {
+            //     console.log("Go to space and delete!!!!!!");
+            // });
         }
     }
 }
 
-
-function onBtnDeleteClick(event, id, imageId) {
-    event.preventDefault();
-
-    if (document.getElementById(id)) {
-        document.getElementById(id).remove();
-    } else {
-       // window.location.href = '/';
-    }
-
-    if (id !== null && id !== undefined && id.length > 0) {
-        fetch('/notes', {
-            method: 'DELETE',
-            body: JSON.stringify({id: id}),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then(res => {
-          // window.location.href = '/';
-        }).finally(() => {
-            console.log("Go to space and delete!!!!!!");
-        });
-    }
-
-    if (imageId !== null && imageId !== undefined && imageId.length > 0) {
-        }).finally(() => {
-            console.log("Go to space and delete!!!!!!");
-        });
-    }
-
-    if (imageId !== null && imageId !== undefined && imageId.length > 0) {
-        fetch('/notes/image', {
-            method: 'DELETE',
-            body: JSON.stringify({imageId: imageId}),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).finally(() => {
-            console.log("Go to space and delete!!!!!!");
-        });
-
-    }
+if (imageId !== null && imageId !== undefined && imageId.length > 0) {
+    fetch('/notes/image', {
+        method: 'DELETE',
+        body: JSON.stringify({imageId: imageId}),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }).finally(() => {
+        console.log("Go to space and delete!!!!!!");
+    });
 }
 
 function onBtnChangeClick(e) {
 
+    function onBtnChangeClick(e) {
+        e.preventDefault();
+        let changeImg = document.getElementById('fileChange');
+        changeImg.click();
+    }
 
-function onBtnChangeClick(e) {
-    e.preventDefault();
-    let changeImg = document.getElementById('fileChange');
-    changeImg.click();
+    function onFileChange(e, id, imageId) {
+        onBtnDeleteClick(e, id, imageId);
+        onFormSubmit(e);
+    }
 }
-
-function onFileChange(e, id, imageId) {
-    onBtnDeleteClick(e, id, imageId);
-    onFormSubmit(e);
-}
-
