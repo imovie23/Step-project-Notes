ab7f39898b257e3de2df007a7289354b9e2578b0
imovie23 <54276075+imovie23@users.noreply.github.com>
2019-10-16 17:47:03 +0300

    Final part List/ contains a problem put/ (#7)

--- Router/default.js
+++ Router/default.js
@@ -1,21 +1,25 @@
 const routers = require('express').Router();
 const Notes = require('../models/notes');
+const Lists = require('../models/lists');
 
 
 module.exports = function () {
     routers.get('/', function (req, res) {
-
-        Notes.find({}, function (err, notes) {
-            if (err) throw err;
+        
+        Promise.all([
+            Notes.find({}),
+            Lists.find({})
+        ]).then(([notes, toDoLists]) => {
             res.render('index.pug', {
                 title: 'Супер классный сайт, построенный на NodeJs',
-                notes: notes
+                notes: notes,
+                toDoLists:toDoLists
             });
-
+        }).catch((err) => {
+            console.log('Error: ', err);
         });
 
     });
-
 
 
     return routers;
--- Router/listsRouter.js
+++ Router/listsRouter.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
 const Lists = require('../models/lists')
+const mongoose = require('mongoose');
 
 module.exports = function () {
 	router.get('/', function (req, res) {
 		res.render('lists.pug', {});
+	});
+
+	router.get('/:listId', function (req, res) {
+
+		const listId = req.params.listId;
+
+		Lists
+			.findById(mongoose.Types.ObjectId(listId), function (err, list) {
+				console.log(list);
+				res.render('detailsList.pug', {
+					title: 'Параметры клиента:',
+					list: list
+				});
+			});
 	});
 
 	router.post('/', function (req, res) {
@@ -19,6 +34,34 @@ module.exports = function () {
 		    res.json(lists)
 		})
 	})
+
+	router.put('/', function (req, res) {
+		const listsData = req.body;
+
+		console.log(listsData);
+
+		Lists
+			.findByIdAndUpdate(listsData.id, {
+				lists: listsData,
+			}, function (err, note) {
+				// if (err) throw err;
+
+			});
+		res.status(201).send(req.body);
+
+	})
+
+	router.delete('/', function (req, res) {
+		let newList = req.body;
+		Lists
+			.findByIdAndRemove(newList.id, function (err,) {
+				if (err) throw err;
+				console.log('User deleted!');
+			});
+		res.status(201).send(req.body);
+	});
+
+
 
 	return router;
 }
